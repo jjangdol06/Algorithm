3696815a67e196ae808fe52db939412164766a4a
jjangdol06 <jiyeon971023@gmail.com>
2021-02-02 17:30:23 +0900

    Revert "quickSort c++ standard library"
    This reverts commit 7e0434e58fdb4d27fe86de9649ded787ba71563c.

--- sort/quickSort.cpp
+++ sort/quickSort.cpp
@@ -20,7 +20,8 @@ void quickSort(int *data, int start, int end){
         while(data[j] >= data[key] && j > start ){
             j--;
         }
-        if(i > j){            temp = data[j];
+        if(i > j){
+            temp = data[j];
             data[j] = data[key];
             data[key] = temp;
         }else{
