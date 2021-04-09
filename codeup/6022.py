59a4c46e762492609f3574b5d2ca9dfcbca4f551
jjangdol06 <jiyeon971023@gmail.com>
2021-04-09 11:07:38 +0900

    20210408 codeup algorithm

---
+++ codeup/6022.py
@@ -0,0 +1,15 @@
+s = input()
+
+print(s[0:2], s[2:4], s[4:])
+
+
+# 다른 풀이
+# yymmdd = str(input())
+# result = []
+# cnt = 0
+#
+# for _ in range(3):
+#     result.append(yymmdd[cnt:cnt+2])
+#     cnt += 2
+#
+# print(" ".join(result))
---
+++ codeup/6026.py
@@ -0,0 +1,4 @@
+a = float(input())
+b = float(input()) #sys.stdin.readline()
+sum = a+b
+print(sum)
